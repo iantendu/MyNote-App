299fa6789e0681fe1d835f17fd3966475684cdb6
iantendu <iantendu@gmail.com>
2022-01-24 11:08:01 +0300

    Repository implementation

---
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/domain/repository/NoteRepository.kt
@@ -0,0 +1,15 @@
+package com.plcoding.cleanarchitecturenoteapp.feature_note.domain.repository
+
+import com.plcoding.cleanarchitecturenoteapp.feature_note.domain.model.Note
+import kotlinx.coroutines.flow.Flow
+
+interface NoteRepository {
+
+    fun getNotes(): Flow<List<Note>>
+
+    suspend fun getNoteById(id:Int): Note?
+
+    suspend fun deleteNote(note: Note)
+
+    suspend fun insertNote(note: Note)
+}
